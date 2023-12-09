cd2211d188ccc1821457f0cb57e4693a040c49bc
Trung Tran <trandinhtrung176@gmail.com>
2023-12-09 20:59:48 +0700

    feat: ls-tree

--- src/core.rs
+++ src/core.rs
@@ -1,5 +1,4 @@
-use hex_literal::hex;
-use sha1::{Digest, Sha1};
+use sha1::Digest;
 use std::{
     fs,
     io::{Read, Write},
@@ -21,6 +20,9 @@ impl App {
         } else if args[1] == "hash-object" {
             let file_path = args[3].clone();
             self.hash_object(&file_path)
+        } else if args[1] == "ls-tree" {
+            let tree_sha = args[3].clone();
+            self.ls_tree(tree_sha)
         }
     }
 
@@ -36,10 +38,11 @@ impl App {
         let subfolder = &blob_sha[0..2];
         let binary_content =
             fs::read(format!(".git/objects/{}/{}", subfolder, &blob_sha[2..])).unwrap();
-        let mut content = String::new();
+        let mut content = Vec::new();
         flate2::read::ZlibDecoder::new(&binary_content[..])
-            .read_to_string(&mut content)
+            .read_to_end(&mut content)
             .unwrap();
+        let content = String::from_utf8_lossy(&content);
         let (_, content) = content.split_once('\x00').unwrap();
         print!("{}", content);
     }
@@ -66,7 +69,62 @@ impl App {
             compressed,
         ) {
             Ok(_) => {}
-            Err(e) => println!("{e}")
+            Err(e) => println!("{e}"),
         }
+    }
+
+    fn ls_tree(&self, tree_sha: String) {
+        let content = self.read_object(&tree_sha);
+        let str_content = String::from_utf8_lossy(&content[..]);
+        let normalized_tree_sha =
+            if content.starts_with(b"tree") && str_content.split_once('\x00').is_none() {
+                let (_, content) = content.split_at(5);
+
+                hex::encode(&content[0..20])
+            } else {
+                tree_sha
+            };
+
+        let content = self.read_object(&normalized_tree_sha);
+
+        let parts = content.split(|x| *x == b'\0');
+
+        let mut entries = parts
+            .skip(1)
+            .enumerate()
+            .filter(|(_, x)| !x.is_empty())
+            .fold(Vec::new(), |mut acc, (i, part)| {
+                if i == 0 {
+                    let (mode, name) = std::str::from_utf8(part).unwrap().split_once(' ').unwrap();
+                    acc.push((mode, name, "".into()));
+                } else {
+                    let sha = &part[0..20];
+                    acc[i - 1].2 = hex::encode(sha);
+
+                    if let Some((mode, name)) =
+                        std::str::from_utf8(&part[20..]).unwrap().split_once(' ')
+                    {
+                        acc.push((mode, name, "".into()))
+                    };
+                }
+
+                acc
+            });
+        entries.sort_by(|a, b| a.1.cmp(b.1));
+        for (_mode, name, _sha) in entries {
+            println!("{}", name)
+        }
+    }
+
+    fn read_object(&self, blob_sha: &str) -> Vec<u8> {
+        let subfolder = &blob_sha[0..2];
+        let binary_content =
+            fs::read(format!(".git/objects/{}/{}", subfolder, &blob_sha[2..])).unwrap();
+        let mut content = Vec::new();
+        flate2::read::ZlibDecoder::new(&binary_content[..])
+            .read_to_end(&mut content)
+            .unwrap();
+
+        content
     }
 }
