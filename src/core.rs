7bd1fafcdfbbc474f510ebc61e11bb74402ad695
Trung Tran <trandinhtrung176@gmail.com>
2023-12-23 21:40:01 +0700

    feat: commit-tree - pass 6th stage

--- src/core.rs
+++ src/core.rs
@@ -1,3 +1,4 @@
+use chrono::{TimeZone, Utc, NaiveDate};
 use sha1::Digest;
 use std::{
     fs,
@@ -26,6 +27,11 @@ impl App {
             self.ls_tree(tree_sha)
         } else if args[1] == "write-tree" {
             self.write_tree()
+        } else if args[1] == "commit-tree" {
+            let tree_sha = args[2].clone();
+            let message = if args.len() == 5 { args[4].clone() } else { args[6].clone() };
+            let parent_hash = if args.len() == 7 { Some(&args[4]) } else { None };
+            self.commit_tree(&tree_sha, &message, parent_hash.map(|x| &x[..]));
         }
     }
 
@@ -107,6 +113,9 @@ impl App {
                     let (mode, name) = std::str::from_utf8(part).unwrap().split_once(' ').unwrap();
                     acc.push((mode, name, "".into()));
                 } else {
+                    if part.len() < 20 {
+                        panic!("Invalid tree entry: {}", String::from_utf8_lossy(&part));
+                    }
                     let sha = &part[0..20];
                     acc[i - 1].2 = hex::encode(sha);
 
@@ -139,58 +148,60 @@ impl App {
 
     fn write_tree(&self) {
         let tree_hash = self.make_tree_object(".");
-        println!("{}", hex::encode(&tree_hash));
+        println!("{}", hex::encode(tree_hash));
     }
 
     fn make_tree_object(&self, path: &str) -> Vec<u8> {
         let mut tree_entries = Vec::new();
         let git_ignore = if let Ok(file) = fs::read(".gitignore") {
-            String::from_utf8(fs::read(".gitignore").unwrap()).unwrap()
+            String::from_utf8(file).unwrap_or_default()
         } else {
             "".into()
         };
 
         if let Ok(entries) = fs::read_dir(path) {
-            entries.filter(|entry| {
-                let file_name = entry.as_ref().unwrap().file_name();
-                if git_ignore.contains(file_name.to_str().unwrap()) {
-                    return false;
-                }
+            entries
+                .filter(|entry| {
+                    let file_name = entry.as_ref().unwrap().file_name();
+                    if git_ignore.contains(file_name.to_str().unwrap()) {
+                        return false;
+                    }
 
-                if file_name.to_str().unwrap() == ".git" {
-                    return false;
-                }
+                    if file_name.to_str().unwrap() == ".git" {
+                        return false;
+                    }
 
-                return true;
-            }).for_each(|entry| {
-                let entry = entry.unwrap();
-                if entry.file_type().unwrap().is_dir() {
-                    let mode = format!("{:o}", 0o40000);
-                    let tree_hash = self.make_tree_object(entry.path().to_str().unwrap());
-                    tree_entries.push((
-                        mode,
-                        entry.file_name().to_string_lossy().to_string(),
-                        tree_hash,
-                    ));
-                } else if entry.file_type().unwrap().is_file() {
-                    let perms = entry.metadata().unwrap().permissions();
-                    let mode = format!("{:o}", perms.mode());
-                    let (_, hash) = self.make_blob_object(entry.path().to_str().unwrap());
-                    tree_entries.push((
-                        mode,
-                        entry.file_name().to_string_lossy().to_string(),
-                        hash,
-                    ));
-                }
-            })
+                    true
+                })
+                .for_each(|entry| {
+                    let entry = entry.unwrap();
+                    if entry.file_type().unwrap().is_dir() {
+                        let mode = format!("{:o}", 0o40000);
+                        let tree_hash = self.make_tree_object(entry.path().to_str().unwrap());
+                        tree_entries.push((
+                            mode,
+                            entry.file_name().to_string_lossy().to_string(),
+                            tree_hash,
+                        ));
+                    } else if entry.file_type().unwrap().is_file() {
+                        let perms = entry.metadata().unwrap().permissions();
+                        let mode = format!("{:o}", perms.mode());
+                        let (_, hash) = self.make_blob_object(entry.path().to_str().unwrap());
+                        tree_entries.push((
+                            mode,
+                            entry.file_name().to_string_lossy().to_string(),
+                            hash,
+                        ));
+                    }
+                })
         }
 
         tree_entries.sort_by(|a, b| a.1.cmp(&b.1));
         let mut content: Vec<u8> = vec![];
 
-        /**
-         * [mode] [file/folder name]\0[SHA-1 of referencing blob or tree]
-         * **/
+        /*
+        [mode] [file/folder name]\0[SHA-1 of referencing blob or tree]
+        */
         for (mode, name, sha) in tree_entries {
             content.extend(format!("{} {}\0", mode, name).as_bytes());
             content.extend(sha);
@@ -220,5 +231,61 @@ impl App {
         }
 
         bin_hash.as_slice().to_vec()
+    }
+
+    fn commit_tree(&self, tree_hash: &str, message: &str, parent_hash: Option<&str>) {
+        let bin_hash = self.make_commit_object(tree_hash, message, parent_hash);
+        let hash = hex::encode(&bin_hash[..]);
+        println!("{}", hash);
+    }
+
+    fn make_commit_object(&self, tree_hash: &str, message: &str, parent_hash: Option<&str>) -> Vec<u8> {
+        let committer_name = "Trung Tran";
+        let committer_email = "trungtran@email.com";
+        let mut content: Vec<u8> = Vec::new();
+        let now = chrono::Local::now();
+        let timestamp = now.timestamp();
+        // let timezone = now.timezone().offset_from_local_date();
+        let offset = now.offset();
+        let hour = offset.local_minus_utc() / 3600;
+        let timezone = format!("{}{:02}00", if hour < 0 {"-"} else {"+"}, hour.abs());
+
+        content.extend(format!("tree {}\n", tree_hash).as_bytes());
+        if let Some(parent_hash) = parent_hash {
+            content.extend(format!("parent {}\n", parent_hash).as_bytes());
+        }
+        content.extend(format!("author {} <{}> {} {}\n", committer_name, committer_email, timestamp, timezone).as_bytes());
+        content.extend(format!("committer {} <{}> {} {}\n\n", committer_name, committer_email, timestamp, timezone).as_bytes());
+        content.extend(format!("{}\n", message).as_bytes());
+        let header= format!("commit {}\0", content.len()).into_bytes();
+        let content = [&header[..], &content[..]].concat();
+        let mut compressed = Vec::new();
+        let mut compressor =
+            flate2::write::ZlibEncoder::new(&mut compressed, flate2::Compression::fast());
+        compressor.write_all(&content).unwrap();
+        compressor.finish().unwrap();
+
+        let mut hasher = sha1::Sha1::new();
+        hasher.update(&content);
+        let bin_hash = hasher.finalize();
+
+        let hash = hex::encode(&bin_hash[..]);
+        let subfolder = &hash[0..2];
+        fs::create_dir_all(format!(".git/objects/{}/", subfolder)).unwrap();
+        match fs::write(
+            format!(".git/objects/{}/{}", subfolder, &hash[2..]),
+            compressed,
+        ) {
+            Ok(_) => {}
+            Err(e) => println!("{e}"),
+        }
+
+        bin_hash.as_slice().to_vec()
+    }
+
+    fn timestamp() -> u128 {
+        let time = std::time::SystemTime::now();
+        let since_the_epoch = time.duration_since(std::time::UNIX_EPOCH).unwrap();
+        since_the_epoch.as_millis()
     }
 }
